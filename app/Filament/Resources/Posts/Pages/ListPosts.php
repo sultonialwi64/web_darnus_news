ce80d11d691e6446eb326d6e2f4b64affd7e2b1a
sultonialwi64 <sultonialwi64@gmail.com>
2026-04-23 14:06:46 +0700

    feat: add draft and published tabs to the news list page

--- app/Filament/Resources/Posts/Pages/ListPosts.php
+++ app/Filament/Resources/Posts/Pages/ListPosts.php
@@ -5,6 +5,8 @@ namespace App\Filament\Resources\Posts\Pages;
 use App\Filament\Resources\Posts\PostResource;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Resources\Pages\ListRecords\Tab;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListPosts extends ListRecords
 {
@@ -14,6 +16,19 @@ class ListPosts extends ListRecords
     {
         return [
             CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'semua' => Tab::make('Semua'),
+            'draft' => Tab::make('Draft')
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('is_published', false))
+                ->icon('heroicon-m-document-text'),
+            'terbit' => Tab::make('Terbit')
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('is_published', true))
+                ->icon('heroicon-m-check-circle'),
         ];
     }
 }
